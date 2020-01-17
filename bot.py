857a78455c613480f6b86ff5b320ef347860dbd6
GFfran <59939732+GFfran@users.noreply.github.com>
2020-01-16 20:04:12 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,15 +11,20 @@ class Bot(object):
     def handle(self, user_id, user_message):
         logging.info("Se logro")
         # Obtener historial de eventos / mensajesç
-        history = [
-            (u"¡Hola! Soy el Bot de MPG. Por favor, escoge una de las opciones para ayudarte.", "bot"),
-            (u"CTS", "user"),
-            (u"Por favor, elija la opcion que desee consultar.", "bot"),
-            (user_message, "user")
-        ]
+        self.users_dao.add_user_event(user_id, 'user', user_message)
+        history = self.users_dao.get_user_events(user_id)
+
+        # history = [
+        #     (u"¡Hola! Soy el Bot de MPG. Por favor, escoge una de las opciones para ayudarte.", "bot"),
+        #     (u"CTS", "user"),
+        #     (u"Por favor, elija la opcion que desee consultar.", "bot"),
+        #     (user_message, "user")
+        # ]
         # En Func. al mensaje escrito por el usuario (y tree)
         response_text = self.tree['say']
         possible_answers = self.tree['answers'].keys()
+        possible_answers.sort()
+
 
         tree = self.tree
 
@@ -27,11 +32,11 @@ class Bot(object):
             logging.info("text : %s", text)
             logging.info("author : %s", author)
             
-            if author == 'bot':
-                if text == tree['say']:
+            if author == "bot":
+                if 'say' in tree && text == tree['say']:
                     tree == tree['answers']
 
-            elif author == 'user':
+            elif author == "user":
                 key = get_key_if_valid(text, tree)
                 if key is not None:
                     tree = tree[key]
@@ -39,12 +44,12 @@ class Bot(object):
                         response_text = tree['say']
                     if 'answers' in tree:
                         possible_answers = tree['answers'].keys()
-
+                        possible_answers.sort()
                     else:
                         possible_answers = None
-                        
-        possible_answers.sort()               
+
         self.send_callback(user_id, response_text, possible_answers)
+        self.users_dao.add_user_event(user_id, 'bot', response_text)
 
 def get_key_if_valid(text, dictionary):
     for key in dictionary:
